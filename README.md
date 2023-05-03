d0d3e989907973f03a58abebe6edd02278ee822f
phaishuk <xomas@ukr.net>
2023-05-03 17:53:10 +0300

    all done

--- README.md
+++ README.md
@@ -1,12 +1,46 @@
 # todo-do-list-django-practice
 
-This is a Django practice project. Here you need to only several commands:
+This is a Django practice project. Here performed the simplest CRUD operations. You need to only several commands:
+
+## Installation
+
+
+> 👉 Download the code 
 
 ```angular2html
-git clone https://github.com/phaishuk/todo-do-list-django-practice.git
-python manage.py migrate
-python manage.py runserver
+git clone https://github.com/phaishuk/todo-do-list-django-practice
+todo-do-list-django-practice
 ```
 
-Thanks for checking this task!
+> 👉 .env file
+
+This project contains some sensitive data, so an `.env` file is required.
+Rename the `.env.sample` file to `.env` or create an empty `.env` 
+and copy the contents of `.env.sample` there.
+
+
+> 👉 Install modules via `VENV`  
+
+```angular2html
+python -m venv venv
+venv\Scripts\activate (on Windows)
+source venv/bin/activate (on Linux / macOS)
+pip install -r requirements.txt
+```
+
+> 👉 Set Up Database
+
+```angular2html
+python manage.py migrate
+```
+
+> 👉 Start the app
+
+```bash
+$ python manage.py runserver
+```
+
+At this point, the app runs at `http://127.0.0.1:8000/`. 
+
+<br />
 
--- tests/test_view.py
+++ tests/test_view.py
@@ -1,13 +1,17 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from to_do_list.models import Tag, Task
+from to_do_list.models import Task, Tag
 
 
 class TaskChangeStatusViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.task = Task.objects.create(content="test", deadline="2023-05-03", is_done=False)
+        self.tag = Tag.objects.create(name="test_tag")
+        self.task = Task.objects.create(
+            content="test", deadline="2023-05-03", is_done=False
+        )
+        self.task.tags.add(self.tag)
 
     def test_task_is_done_toggle(self):
         url = reverse('to_do_list:task-change-is-done', args=[self.task.id])
@@ -29,11 +33,17 @@ class TaskChangeStatusViewTest(TestCase):
 
 
 class TaskCreateViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.tag = Tag.objects.create(name="test_tag")
+
     def test_task_create_view_success(self):
         data = {
             "content": "New task content",
             "deadline": "2022-01-01 12:00:00",
-            "is_done": False
+            "is_done": False,
+            "tags": [self.tag.pk]
         }
         response = self.client.post(reverse("to_do_list:task-create"), data=data)
         self.assertEqual(response.status_code, 302)
@@ -43,28 +53,31 @@ class TaskCreateViewTestCase(TestCase):
         data = {
             "content": "",
             "deadline": "2022-01-01 12:00:00",
-            "is_done": False
+            "is_done": False,
+            "tags": [self.tag.pk]
         }
         response = self.client.post(reverse("to_do_list:task-create"), data=data)
-        self.assertEqual(response.status_code,
-                         200)
+        self.assertEqual(response.status_code, 200)
         self.assertFalse(Task.objects.filter(content="").exists())
 
 
 class TaskUpdateViewTestCase(TestCase):
     def setUp(self):
+        self.tag = Tag.objects.create(name="test_tag")
         self.task = Task.objects.create(
             content="Test task content",
             deadline="2022-01-01 12:00:00",
             is_done=False
         )
+        self.task.tags.add(self.tag)
         self.url = reverse("to_do_list:task-update", kwargs={"pk": self.task.pk})
 
     def test_task_update_view_success(self):
         data = {
             "content": "Updated task content",
             "deadline": "2022-01-02 12:00:00",
-            "is_done": True
+            "is_done": True,
+            "tags": [self.tag.pk],
         }
         response = self.client.post(self.url, data=data)
         self.assertEqual(response.status_code, 302)
@@ -75,10 +88,59 @@ class TaskUpdateViewTestCase(TestCase):
         data = {
             "content": "",
             "deadline": "2022-01-01 12:00:00",
-            "is_done": False
+            "is_done": False,
+            "tags": [self.tag.pk],
         }
         response = self.client.post(self.url, data=data)
         self.assertEqual(response.status_code, 200)
         self.task.refresh_from_db()
         self.assertNotEqual(self.task.content, "")
 
+
+class TagCreateViewTestCase(TestCase):
+
+    def test_tag_create_view_success(self):
+        response = self.client.post(reverse("to_do_list:tag-create"), data={"name": "test_tag"})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Tag.objects.filter(name="test_tag").exists())
+
+    def test_tag_create_view_missing_required_fields(self):
+        response = self.client.post(reverse("to_do_list:tag-create"), data={})
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(Tag.objects.filter(name="").exists())
+
+
+class TagUpdateViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.tag = Tag.objects.create(name="test_tag")
+        self.url = reverse("to_do_list:tag-update", kwargs={"pk": self.tag.pk})
+
+    def test_task_update_view_success(self):
+        response = self.client.post(self.url, data={"name": "test_tag2"})
+        self.assertEqual(response.status_code, 302)
+        self.tag.refresh_from_db()
+        self.assertEqual(self.tag.name, "test_tag2")
+
+    def test_task_update_view_missing_required_fields(self):
+        response = self.client.post(self.url, data={"name": ""})
+        self.assertEqual(response.status_code, 200)
+        self.tag.refresh_from_db()
+        self.assertNotEqual(self.tag.name, "")
+
+
+class TaskListViewTestCase(TestCase):
+
+    def test_task_list_view(self):
+        response = self.client.get(reverse("to_do_list:task-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "to_do_list/task_list.html")
+
+
+class TagListViewTestCase(TestCase):
+
+    def test_tag_list_view(self):
+        response = self.client.get(reverse("to_do_list:tag-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "to_do_list/tag_list.html")
